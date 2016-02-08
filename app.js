27b92148af3ef9dbed5043cf96505844efd08e22
put your name or nickname here <put@your.email.address.here>
2016-02-08 22:46:00 +0200

    home updated

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
+var Kinect2 = require('kinect2');
 var express = require('express');
 var session = require('express-session');
 var path = require('path');
 var validator = require('express-validator');
-{
-  /*var favicon = require('serve-favicon');
-   var logger = require('morgan');
-   var cookieParser = require('cookie-parser');
-   var bodyParser = require('body-parser');*/
 
-}
-
-//-->> To Go to Web Pages Page require the index.js
 var routes = require('./routes/index');
 var db= require('./db');
-
+var kinect = new Kinect2();
 
 var app = express();
+var server = require('http').createServer(app);
+
+var io = require('socket.io').listen(server);
 var db = require('./db');
-//app.use(express.cookieParser());
+
 app.use(session({
   secret: 'Session',
   //name: cookie_name,
@@ -30,32 +26,17 @@ app.use(session({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-{
-  // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-  /*app.use(logger('dev'));
-   app.use(bodyParser.json());
-   app.use(bodyParser.urlencoded({ extended: false }));
-   app.use(cookieParser());*/
 
-}
 app.use(express.static(path.join(__dirname, 'public')));
 
-// -->> To Route and Display Pages according response of index
 app.use('/', routes);
-
-console.log("http://localhost:3000/");
-// catch 404 and forward to error handler
+/*
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -66,8 +47,6 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -75,6 +54,19 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+*/
+if(kinect.open()) {
+  server.listen(3000);
+  console.log('Server listening on port 3000');
+  console.log('Point your browser to http://localhost:3000');
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use('/', routes);
 
+  kinect.on('bodyFrame', function(bodyFrame){
+    io.sockets.emit('bodyFrame', bodyFrame);
+  });
+
+  kinect.openBodyReader();
+}
 
 module.exports = app;
